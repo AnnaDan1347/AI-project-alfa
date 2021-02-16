5ad1d8653c2c0cce958bdaaeccdc273bee84f751
AnnaDan1347 <52747391+AnnaDan1347@users.noreply.github.com>
2021-02-16 08:26:02 +0300

    Update script.cpp
    change script.cpp

--- script.cpp
+++ script.cpp
@@ -9,12 +9,26 @@
 using namespace std;
 using namespace cgicc;
 
-int getInt(string name);//get length of text putted in the form on your site(this function we use only for example)
+#define DEPTH 4
+
+using namespace std;
+
+string textConvertation(string text);
+int howManyWordsTextHas(string text);
+void fillingArray(string text, string array[]);
+double calculatingPercentageOfPlagiarism(string sourceTextArray[], string verifiableTextArray[], int sizeOfSourceText, int sizeOfVerifiableArray);
+void printIntro();
+void printPercentage(double percentage);
+double antiPlagiarism(string text, string fragment);
+
+//int getInt(string name);//get length of text putted in the form on your site(this function we use only for example)
 string getDB();//get origin text from db.txt (don't modify tis function)
 
 int main()
 {
-    Cgicc form;
+    
+
+	Cgicc form;
     string name;
 
     cout << "Content-type:text/html\r\n\r\n";
@@ -27,7 +41,7 @@ int main()
     
     name = form("name");
     if (!name.empty()) {
-    	cout << getInt(name) << "\n";
+    	cout << antiPlagiarism(getDB(), name) << "\n";
     } else {
     	cout << "Text is not provided!\n";
     }	
@@ -36,9 +50,6 @@ int main()
     cout << "</html>\n";
 
     return 0;
-}
-int getInt(string name){
-	return name.length();
 }
 
 string getDB(){
@@ -51,4 +62,118 @@ string getDB(){
     in.close();
     
     return dbText;
+}
+
+double antiPlagiarism(string sourceText, string verifiableText) {
+	printIntro();
+	
+	
+	sourceText = textConvertation(sourceText);
+	verifiableText = textConvertation(verifiableText);
+	
+	int amountOfWordsInSourceText = howManyWordsTextHas(sourceText);
+	int amountOfWordsInVerifiableText = howManyWordsTextHas(verifiableText);
+	
+	string sourceTextArray[amountOfWordsInSourceText];
+	string verifiableTextArray[amountOfWordsInVerifiableText];
+	
+	fillingArray(sourceText, sourceTextArray);
+	fillingArray(verifiableText, verifiableTextArray);
+	
+	double percentageOfPlagiarism = calculatingPercentageOfPlagiarism(sourceTextArray, verifiableTextArray, amountOfWordsInSourceText, amountOfWordsInVerifiableText);
+	
+	printPercentage(percentageOfPlagiarism);
+
+};
+
+string textConvertation(string text){
+	string temp;
+	
+	//ASCII 
+	for(int i = 0; i < text.size(); i++){
+		if(text[i] == ' '){
+			temp += ' ';
+		}else if(text[i] >= 65 and text[i] <= 90){
+			temp += (text[i] + 32);
+		}else if((text[i] >= 48 and text[i] <= 57) or (text[i] >= 97 and text[i] <= 122)){
+			temp += text[i];
+		}
+	}
+	
+	return temp;
+}
+
+int howManyWordsTextHas(string text){
+	int counter = 1;
+	
+	for(int i = 0; text[i] != '\0'; i++){
+		if(text[i] == ' '){
+			counter++;
+		}
+	}
+	
+	if(counter < DEPTH){
+	//cout << "You inputed too short string!" << endl;
+		
+		return - 1;
+	}
+	
+	return counter;
+}
+
+void fillingArray(string text, string array[]){
+	int wordsCounter = 0;
+	string word;
+	
+	for(int i = 0; i <= text.size(); i++){
+		if(text[i] == ' ' or (i == text.size())){
+			array[wordsCounter] = word;
+			wordsCounter++;
+ 			word = "";
+		}else{
+    		word += text[i];
+    	}
+	}
+}
+
+double calculatingPercentageOfPlagiarism(string sourceTextArray[], string verifiableTextArray[], int sizeOfSourceText, int sizeOfVerifiableText){
+	int allSelections = 0, plagiatedSelections = 0;
+	int stepForSourceText = 0, stepForVerifiableText = 0;
+	string tempForSourceText, tempForVerifiableText;
+	
+	for(int i = 0; i < ((sizeOfVerifiableText - DEPTH) + 1); i++){
+		for(int j = 0; j < ((sizeOfSourceText - DEPTH) + 1); j++){
+			for(int k = 0; k < DEPTH; k++){
+				tempForVerifiableText += verifiableTextArray[k + stepForVerifiableText];
+				tempForSourceText += sourceTextArray[k + stepForSourceText];
+			}
+
+			if(tempForVerifiableText == tempForSourceText){
+				plagiatedSelections++;
+				
+				tempForVerifiableText = "";
+				tempForSourceText  = "";
+			
+				break;
+			}
+			
+			stepForSourceText++;
+			tempForVerifiableText = "";
+			tempForSourceText  = "";
+		}
+		allSelections++;
+		stepForVerifiableText++;
+		stepForSourceText = 0;
+	}
+
+	return (plagiatedSelections * 100.00) / allSelections;
+	
+}
+
+void printIntro(){
+	//cout << "Hello, user! This program helps to calculate the percentage of plagiarism." << endl << endl;
+}
+
+void printPercentage(double percentage){
+	//cout << endl << "Plagiarism percentage: " << percentage << "%";
 }
